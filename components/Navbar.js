e6b9ee4ebd137b0a6440194ce859d8fa138fd880
Abhyuday Tripathi <tripathi.abhyuday2010@gmail.com>
2021-05-05 22:02:10 +0530

    Initial Commit

---
+++ components/Navbar.js
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { useContext } from 'react';
+import AuthContext from '../stores/authContext';
+
+export default function Navbar() {
+  const { user, login, logout, authReady } = useContext(AuthContext);
+
+  return (
+    <div className='container'>
+      <nav>
+        <Image src='/logo_transparent.png' width={50} height={48} />
+        <h1>DailyDIY</h1>
+        {authReady && (
+          <ul>
+            <li>
+              <Link href='/'>
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/guides'>
+                <a>Guides</a>
+              </Link>
+            </li>
+            {!user && (
+              <li onClick={login} className='btn'>
+                Login/Signup
+              </li>
+            )}
+            {user && <li>{user.email}</li>}
+            {user && (
+              <li onClick={logout} className='btn'>
+                Logout
+              </li>
+            )}
+          </ul>
+        )}
+      </nav>
+      <div className='banner'>
+        <Image src='/banner2.png' width={1536} height={768} />
+      </div>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.home}>
+      <h2>Welcome to DailyDIY</h2>
+      <div>
+        <p>
+          Want tips and tricks? You are at the right place! DailyDIY is a online
+          Blog Website where you can solve your problems by just reading!
+        </p>
+      </div>
+    </div>
+  );
+}
